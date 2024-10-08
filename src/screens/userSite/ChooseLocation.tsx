41efaf7c11145f53171b6a84d1751a615b25a0b4
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-10-08 23:26:13 +0500

    google map integrated

--- src/screens/userSite/ChooseLocation.tsx
+++ src/screens/userSite/ChooseLocation.tsx
@@ -1,9 +1,69 @@
-import {Dimensions, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  Alert,
+  PermissionsAndroid,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React, {useEffect} from 'react';
 import MapView from 'react-native-maps';
 import {Marker} from 'react-native-maps';
+// import Geolocation from '@react-native-community/geolocation';
+import Geolocation from 'react-native-geolocation-service';
 
 const ChooseLocation = () => {
+  const getUsersCurrentLocation = () => {
+    // Geolocation.getCurrentPosition(position => {
+    //   console.log(position);
+    // });
+    Geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+      },
+      error => {
+        // See error code charts below.
+        console.log(error.code, error.message);
+      },
+      {enableHighAccuracy: false, timeout: 15000, maximumAge: 10000},
+    );
+  };
+
+  const requestLocationPermission = async () => {
+    if (Platform.OS === 'android') {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+          {
+            title: 'Cool Photo App Camera Permission',
+            message:
+              'Cool Photo App needs access to your camera ' +
+              'so you can take awesome pictures.',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK',
+          },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('granted');
+          getUsersCurrentLocation();
+        } else {
+          Alert.alert(
+            'Permission Denied',
+            'Location permission is required to show your current location on the map.',
+          );
+        }
+      } catch (err) {
+        console.warn(err);
+      }
+    } else {
+    }
+  };
+
+  useEffect(() => {
+    requestLocationPermission();
+  }, []);
+
   return (
     <View style={{flex: 1}}>
       <MapView
