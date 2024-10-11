7a8901df3b686dd1f5502fb7f71b7524b3d223d1
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-10-11 22:47:47 +0500

    working on distance calculation on map

--- src/screens/userSite/ChooseLocation.tsx
+++ src/screens/userSite/ChooseLocation.tsx
@@ -1,5 +1,6 @@
 import {
   Alert,
+  Button,
   PermissionsAndroid,
   Platform,
   StyleSheet,
@@ -7,17 +8,29 @@ import {
   View,
 } from 'react-native';
 import React, {useEffect, useState} from 'react';
-import MapView, {Region} from 'react-native-maps';
+import MapView, {
+  Region,
+  MapPressEvent,
+  LatLng,
+  Polyline,
+} from 'react-native-maps';
 import {Marker} from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
 
 const ChooseLocation = () => {
-  const [location, setLocation] = useState<object>({
+  const [location, setLocation] = useState<Region>({
     latitude: 0,
     longitude: 0,
     latitudeDelta: 0,
-    longtudeDelta: 0,
+    longitudeDelta: 0,
   });
+  const [source, setSource] = useState<LatLng>({latitude: 0, longitude: 0});
+  const [destination, setDestination] = useState<LatLng>({
+    latitude: 0,
+    longitude: 0,
+  });
+  const [isChoosingSource, setIsChoosingSource] = useState(false);
+  const [isChoosingDestination, setIsChoosingDestination] = useState(false);
 
   const defaultLocation = {
     latitude: 37.78825,
@@ -34,7 +47,7 @@ const ChooseLocation = () => {
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
           latitudeDelta: 0.01,
-          longtudeDelta: 0.01,
+          longitudeDelta: 0.01,
         });
       },
       error => {
@@ -50,10 +63,10 @@ const ChooseLocation = () => {
         const granted = await PermissionsAndroid.request(
           PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
           {
-            title: 'Cool Photo App Camera Permission',
+            title: 'Map Permission',
             message:
-              'Cool Photo App needs access to your camera ' +
-              'so you can take awesome pictures.',
+              'react native maps needs access to your location ' +
+              'so you can set your location.',
             buttonNeutral: 'Ask Me Later',
             buttonNegative: 'Cancel',
             buttonPositive: 'OK',
@@ -75,28 +88,77 @@ const ChooseLocation = () => {
     }
   };
 
+  const handleMapPress = (e: MapPressEvent) => {
+    const coordinates = e.nativeEvent.coordinate;
+    console.log(coordinates);
+    if (isChoosingSource) {
+      setSource(coordinates);
+      setIsChoosingSource(false);
+    } else if (isChoosingDestination) {
+      setDestination(coordinates);
+      setIsChoosingDestination(false);
+    }
+  };
+
+  const showCoordinates = () => {
+    console.log(source, destination);
+  };
+
   useEffect(() => {
     requestLocationPermission();
   }, []);
 
   return (
-    <View style={{flex: 1}}>
+    <View style={styles.container}>
       <MapView
         style={styles.map}
-        region={location as Region}
-        onRegionChangeComplete={data => console.log(data)}>
-        <Marker
-          coordinate={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            // latitudeDelta: 0.0922,
-            // longitudeDelta: 0.0421,
-          }}
-          onPress={data => console.log(data.nativeEvent.coordinate)}
-          title={'Testing'}
-          // description={marker.description}
-        />
+        region={location}
+        showsUserLocation={true}
+        // onRegionChangeComplete={data => console.log(data)}
+        onPress={handleMapPress}>
+        {location && (
+          <Marker
+            coordinate={location}
+            onPress={data => console.log(data.nativeEvent.coordinate)}
+            title={'Testing'}
+            // description={marker.description}
+          />
+        )}
+        {source && (
+          <Marker coordinate={source} title={'Source'} pinColor={'green'} />
+        )}
+        {destination && (
+          <Marker
+            coordinate={destination}
+            title={'Destination'}
+            pinColor={'blue'}
+          />
+        )}
+        {source && destination && (
+          <Polyline
+            coordinates={[source, destination]}
+            strokeColor="#000"
+            strokeWidth={2}
+          />
+        )}
       </MapView>
+      <View style={styles.buttonContainer}>
+        <View style={styles.buttonGroup}>
+          <Button
+            title={isChoosingSource ? 'Please Choose Source' : 'Choose Source'}
+            onPress={() => setIsChoosingSource(true)}
+          />
+          <Button
+            title={
+              isChoosingDestination
+                ? 'Please Choose Destination'
+                : 'Choose Destination'
+            }
+            onPress={() => setIsChoosingDestination(true)}
+          />
+        </View>
+        <Button title="Show Coordinates" onPress={showCoordinates} />
+      </View>
     </View>
   );
 };
@@ -106,14 +168,21 @@ export default ChooseLocation;
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   map: {
-    // width: Dimensions.get('window').width,
-    // height: Dimensions.get('window').height,
     ...StyleSheet.absoluteFillObject,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    right: 20,
+  },
+  buttonGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
   },
 });
