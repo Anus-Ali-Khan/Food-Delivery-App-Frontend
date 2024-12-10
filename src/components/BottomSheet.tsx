84057146bd504a6b187d2700968d73ba4d6bf678
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-12-10 10:49:16 +0500

    feat: bottom sheet integrated

--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -1,19 +1,89 @@
-import React, {useCallback, useMemo, useRef} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useCallback, useMemo, useRef, useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  PermissionsAndroid,
+} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import BottomSheet, {BottomSheetView} from '@gorhom/bottom-sheet';
+import CameraIcon from 'react-native-vector-icons/Feather';
+import {colors, fonts} from '../utilities/constants';
+import GalleryIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
-const CustomBottomSheet = () => {
+interface propsType {
+  isBSOpen: boolean;
+  setIsBSOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  photo: string;
+  setPhoto: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const CustomBottomSheet = ({
+  isBSOpen,
+  setIsBSOpen,
+  photo,
+  setPhoto,
+}: propsType) => {
   const bottomSheetRef = useRef(null);
   const handleSheetChanges = useCallback((index: number) => {
     console.log('handleSheetChanges', index);
   }, []);
 
+  const handleCamera = async () => {
+    setIsBSOpen(false);
+
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        {
+          title: 'App Camera Permission',
+          message: 'App needs access to your camera ',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        const result = await launchCamera({
+          mediaType: 'photo',
+          cameraType: 'back',
+        });
+        if (result) {
+          const photo = (result.assets ?? [])[0].uri;
+          // setPhoto(photo);
+          console.log(photo);
+        }
+        console.log('Camera permission given');
+      } else {
+        console.log('Camera permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
   return (
     <GestureHandlerRootView style={styles.container}>
       <BottomSheet ref={bottomSheetRef} onChange={handleSheetChanges}>
         <BottomSheetView style={styles.contentContainer}>
-          <Text>Awesome 🎉</Text>
+          <TouchableOpacity
+            style={{
+              alignItems: 'center',
+              gap: 4,
+            }}>
+            <CameraIcon name="camera" size={30} onPress={handleCamera} />
+            <Text style={[styles.textStyle]}>Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{alignItems: 'center', gap: 4}}>
+            <GalleryIcon
+              name="view-gallery-outline"
+              size={30}
+              onPress={() => setIsBSOpen(false)}
+            />
+            <Text style={styles.textStyle}>Gallery</Text>
+          </TouchableOpacity>
         </BottomSheetView>
       </BottomSheet>
     </GestureHandlerRootView>
@@ -26,10 +96,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'grey',
+    // position: 'absolute',
+    // bottom: 0,
+    // right: 0,
+    zIndex: 1000,
   },
   contentContainer: {
     flex: 1,
     padding: 36,
     alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  textStyle: {
+    fontFamily: fonts.SECONDARY,
   },
 });
--- src/components/Navigations/DrawerNavigation.tsx
+++ src/components/Navigations/DrawerNavigation.tsx
@@ -24,202 +24,158 @@ import Settings from '../../screens/userSite/Settings';
 import SettingsIcon from 'react-native-vector-icons/Ionicons';
 import {ParamListBase, useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
-import CustomModal from '../CustomModal';
-import Button from '../Button';
+
 import BottomSheet from '../BottomSheet';
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigation = () => {
   const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
-  const [photo, setPhoto] = useState<string>();
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-
-  const handleCamera = async () => {
-    // setIsModalOpen(true);
-
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.CAMERA,
-        {
-          title: 'App Camera Permission',
-          message: 'App needs access to your camera ',
-          buttonNeutral: 'Ask Me Later',
-          buttonNegative: 'Cancel',
-          buttonPositive: 'OK',
-        },
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        const result = await launchCamera({
-          mediaType: 'photo',
-          cameraType: 'back',
-        });
-        if (result) {
-          const photo = (result.assets ?? [])[0].uri;
-          setPhoto(photo);
-          console.log(photo);
-        }
-        console.log('Camera permission given');
-      } else {
-        console.log('Camera permission denied');
-      }
-    } catch (err) {
-      console.warn(err);
-    }
-  };
+  const [isBSOpen, setIsBSOpen] = useState<boolean>(false);
+  const [photo, setPhoto] = useState<string>('');
 
   return (
-    <Drawer.Navigator
-      initialRouteName="TopTabNavigation"
-      drawerContent={props => {
-        return (
-          <SafeAreaView style={{flex: 1}}>
-            {/* {isModalOpen && (
-              <CustomModal
-                isOpen={isModalOpen}
-                children={
-                  <View style={styles.modalStyle}>
-                    <TouchableOpacity style={styles.modalButtonStyle}>
-                      <Text style={styles.textStyle}>Open Camera</Text>
+    <View style={{flex: 1}}>
+      <Drawer.Navigator
+        initialRouteName="TopTabNavigation"
+        drawerContent={props => {
+          return (
+            <SafeAreaView style={{flex: 1}}>
+              <DrawerContentScrollView>
+                <View
+                  style={{
+                    height: 200,
+                    width: '100%',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    marginTop: 40,
+                    marginBottom: 24,
+                  }}>
+                  <View>
+                    <TouchableOpacity
+                      style={{
+                        position: 'absolute',
+                        backgroundColor: colors.SECONDARY,
+                        borderRadius: 65,
+                        padding: 8,
+                        bottom: 0,
+                        right: 30,
+                        zIndex: 1,
+                      }}>
+                      <CameraIcon
+                        name="camera"
+                        color={'white'}
+                        onPress={() => setIsBSOpen(true)}
+                      />
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.modalButtonStyle}>
-                      <Text style={styles.textStyle}>Open gallery</Text>
-                    </TouchableOpacity>
-                    <View style={{flexDirection:'row',justifyContent:'space-between',marginTop:10}}>
-                    <Button title='Cancel' borderColor='transparent' textColor='white'  backgroundColor={colors.SECONDARY} style={{width:100}} />
-                    <Button title='Ok'borderColor='transparent' textColor='white' backgroundColor={colors.SECONDARY} style={{width:100}}/>
+                    <View
+                      style={{
+                        width: 170,
+                        height: 170,
+                        backgroundColor: 'white',
+                        borderRadius: '100%',
+                      }}>
+                      <Image src={photo} height={170} width={170} />
                     </View>
                   </View>
-                }
-              />
-            )} */}
-            <DrawerContentScrollView>
-              <View
+                  <Text
+                    style={{
+                      fontSize: 26,
+                      color: colors.SECONDARY,
+                      fontFamily: fonts.SECONDARY,
+                      marginTop: 10,
+                    }}>
+                    Abu Bin Hayan
+                  </Text>
+                </View>
+                <DrawerItemList {...props} />
+              </DrawerContentScrollView>
+              <TouchableOpacity
+                onPress={() => navigation.navigate('Settings')}
                 style={{
-                  height: 200,
-                  width: '100%',
                   justifyContent: 'center',
                   alignItems: 'center',
-                  marginTop: 40,
-                  marginBottom: 24,
+                  marginBottom: 20,
                 }}>
-                <View>
-                  <TouchableOpacity
-                    style={{
-                      position: 'absolute',
-                      backgroundColor: colors.SECONDARY,
-                      borderRadius: 65,
-                      padding: 8,
-                      bottom: 0,
-                      right: 30,
-                      zIndex: 1,
-                    }}>
-                    <CameraIcon
-                      name="camera"
-                      color={'white'}
-                      onPress={handleCamera}
-                    />
-                  </TouchableOpacity>
-                  <View
-                    style={{
-                      width: 170,
-                      height: 170,
-                      backgroundColor: 'white',
-                      borderRadius: '100%',
-                    }}>
-                    <Image src={photo} height={170} width={170} />
-                  </View>
-                </View>
+                <SettingsIcon
+                  name="settings-sharp"
+                  color={colors.SECONDARY}
+                  size={25}
+                />
                 <Text
                   style={{
-                    fontSize: 26,
                     color: colors.SECONDARY,
+                    fontSize: 20,
                     fontFamily: fonts.SECONDARY,
-                    marginTop: 10,
                   }}>
-                  Abu Bin Hayan
+                  Settings
                 </Text>
-              </View>
-              <DrawerItemList {...props} />
-            </DrawerContentScrollView>
-            <TouchableOpacity
-              onPress={() => navigation.navigate('Settings')}
-              style={{
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginBottom: 20,
-              }}>
-              <SettingsIcon
-                name="settings-sharp"
-                color={colors.SECONDARY}
-                size={25}
-              />
-              <Text
-                style={{
-                  color: colors.SECONDARY,
-                  fontSize: 20,
-                  fontFamily: fonts.SECONDARY,
-                }}>
-                Settings
-              </Text>
-            </TouchableOpacity>
-          </SafeAreaView>
-        );
-      }}
-      screenOptions={{
-        drawerStyle: {
-          backgroundColor: `${colors.PRIMARY}`,
-          borderTopRightRadius: 20,
-          borderBottomRightRadius: 20,
-        },
-        drawerItemStyle: {
-          // backgroundColor: `${colors.SECONDARY}`,
-        },
-        drawerLabelStyle: {
-          fontSize: 16,
-          fontFamily: `${fonts.SECONDARY}`,
-          fontWeight: 'bold',
-        },
-        drawerActiveTintColor: 'white',
-        drawerActiveBackgroundColor: `${colors.SECONDARY}`,
-        drawerInactiveTintColor: `${colors.SECONDARY}`,
-      }}>
-      <Drawer.Screen
-        name="Home"
-        component={TopTabNavigation}
-        options={{
-          headerTitle: props => <CustomHeader />,
-          headerTitleContainerStyle: {width: '100%'},
+              </TouchableOpacity>
+            </SafeAreaView>
+          );
         }}
-      />
-      <Drawer.Screen
-        name="Order"
-        component={TopTabNavigation}
-        options={{
-          headerTitle: props => <CustomHeader />,
-          headerTitleContainerStyle: {width: '100%'},
-        }}
-      />
-      <Drawer.Screen
-        name="Info"
-        component={Info}
-        options={{
-          headerTitle: props => <CustomHeader />,
-          headerTitleContainerStyle: {width: '100%'},
-        }}
-      />
-      <Drawer.Screen name="Logout" component={LogoutModal} />
-      <Drawer.Screen
-        name="Settings"
-        component={Settings}
-        options={{
-          drawerItemStyle: {
-            display: 'none',
+        screenOptions={{
+          drawerStyle: {
+            backgroundColor: `${colors.PRIMARY}`,
+            borderTopRightRadius: 20,
+            borderBottomRightRadius: 20,
           },
-        }}
-      />
-    </Drawer.Navigator>
+          drawerItemStyle: {
+            // backgroundColor: `${colors.SECONDARY}`,
+          },
+          drawerLabelStyle: {
+            fontSize: 16,
+            fontFamily: `${fonts.SECONDARY}`,
+            fontWeight: 'bold',
+          },
+          drawerActiveTintColor: 'white',
+          drawerActiveBackgroundColor: `${colors.SECONDARY}`,
+          drawerInactiveTintColor: `${colors.SECONDARY}`,
+        }}>
+        <Drawer.Screen
+          name="Home"
+          component={TopTabNavigation}
+          options={{
+            headerTitle: props => <CustomHeader />,
+            headerTitleContainerStyle: {width: '100%'},
+          }}
+        />
+        <Drawer.Screen
+          name="Order"
+          component={TopTabNavigation}
+          options={{
+            headerTitle: props => <CustomHeader />,
+            headerTitleContainerStyle: {width: '100%'},
+          }}
+        />
+        <Drawer.Screen
+          name="Info"
+          component={Info}
+          options={{
+            headerTitle: props => <CustomHeader />,
+            headerTitleContainerStyle: {width: '100%'},
+          }}
+        />
+        <Drawer.Screen name="Logout" component={LogoutModal} />
+        <Drawer.Screen
+          name="Settings"
+          component={Settings}
+          options={{
+            drawerItemStyle: {
+              display: 'none',
+            },
+          }}
+        />
+      </Drawer.Navigator>
+      {isBSOpen && (
+        <BottomSheet
+          isBSOpen={isBSOpen}
+          setIsBSOpen={setIsBSOpen}
+          photo={photo}
+          setPhoto={setPhoto}
+        />
+      )}
+    </View>
   );
 };
 
